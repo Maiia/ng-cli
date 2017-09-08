346066a2b8c26fb084b5df81e207f36bc2e3a689
LEVI9\l.kuzenko <lubko.lintu@gmail.com>
2017-09-08 13:04:31 +0300

    add bootstrap 4 and font-awersome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public ngOnInit() {
+    console.log('hello `Home` component');
+    $('[data-toggle="tooltip"]').tooltip();
+  }
 }
