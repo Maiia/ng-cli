1e8ef9cc5c048317e44177a0986cc90b4ebcd70b
Liubomyr Kuzenko <lubko.lintu@gmail.com>
2018-05-14 16:47:27 +0300

    add tests for 404 page

---
+++ src/app/no-content/no-content.component.spec.ts
@@ -0,0 +1,42 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { NoContentComponent } from './no-content.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+describe('NoContentComponent: ', () => {
+  let headerElement: HTMLElement;
+  let component: NoContentComponent;
+  let fixture: ComponentFixture<NoContentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NoContentComponent ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NoContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it(`should be readly initialized`, () => {
+    expect(fixture).toBeDefined();
+    expect(component).toBeDefined();
+  });
+
+  it(`should be readly initialized and log some information`, () => {
+    spyOn(console, 'log');
+    expect(console.log).not.toHaveBeenCalled()
+
+    component.ngOnInit()
+
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it(`should have title '404 Not Found'`, () => {
+    headerElement = <HTMLElement> fixture.nativeElement.querySelector("h1");
+    expect(headerElement.innerText).toContain('404 Not Found')
+  })
+});
